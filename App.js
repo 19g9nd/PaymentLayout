21453e16cceea0f66a52e54a4af1e4de6d4e75bf
19g9nd <ahadova2017@gmail.com>
2024-09-13 23:35:02 +0400

    Add Detailed Data

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import SystemNavigationBar from 'react-native-system-navigation-bar';
 import {
   SafeAreaView,
-  ScrollView,
   StyleSheet,
   Text,
-  Pressable,
+  FlatList,
   View,
   StatusBar,
   Image,
+  TouchableOpacity,
 } from 'react-native';
 
 const show = async () => {
@@ -17,6 +17,63 @@ const show = async () => {
   console.log('Show: ', result);
 };
 show();
+
+const transactionsData = [
+  {
+    id: '1',
+    title: 'Spotify Premium',
+    time: '2 minutes ago',
+    amount: 20,
+  },
+  {
+    id: '2',
+    title: 'Steam Top Up',
+    time: '5 days ago',
+    amount: 150,
+  },
+  {
+    id: '3',
+    title: 'Amazon',
+    time: '1 week ago',
+    amount: 57,
+  },
+  {
+    id: '4',
+    title: 'Dribble Pro',
+    time: '1 week ago',
+    amount: 110,
+  },
+  {
+    id: '5',
+    title: 'Epic Games',
+    time: '2 minutes ago',
+    amount: 200,
+  },
+];
+
+const renderItem = ({item}) => (
+  <View style={styles.transaction}>
+    <View style={styles.profile}>
+      <View style={styles.imgContainer}>
+        <View style={styles.galleryContainer}>
+          <Image
+            style={styles.galleryImg}
+            source={require('./assets/gallery.png')}
+          />
+        </View>
+      </View>
+
+      <View style={styles.transactionMainInfo}>
+        <Text style={styles.transactionText}>{item.title}</Text>
+        <Text style={styles.transactionTime}>{item.time}</Text>
+      </View>
+    </View>
+
+    <View style={styles.transactionDetails}>
+      <Text style={styles.transactionAmount}>${item.amount}</Text>
+    </View>
+  </View>
+);
 
 const App = () => {
   return (
@@ -46,14 +103,35 @@ const App = () => {
         <View style={styles.balanceContainer}>
           <Text style={styles.currency}>Current Balance</Text>
           <Text style={styles.currentBalance}>
-            <Text style={styles.dollarSign}>$</Text>
-            7429,23
+            <Text style={styles.dollarSign}>$</Text>7429,23
           </Text>
-          <Image
-            style={styles.lineImg}
-            source={require('./assets/line.png')}
-          />
+          <View style={styles.line} />
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>
+                {' '}
+                <Image
+                  style={styles.interractImg}
+                  source={require('./assets/plus.png')}
+                />{' '}
+                Add New Card
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>
+                {' '}
+                <Image
+                  style={styles.interractImg}
+                  source={require('./assets/transfer.png')}
+                />{' '}
+                Change Card
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
+        <View style={styles.divider}></View>
       </View>
 
       <View style={styles.body}>
@@ -61,12 +139,13 @@ const App = () => {
           <Text style={styles.sectionTitle}>Recent Transaction</Text>
           <Text style={styles.linkText}>Show All</Text>
         </View>
-        <View>
-          <Text style={styles.transactionText}>SPOTIFY</Text>
-        </View>
+        <FlatList
+          data={transactionsData}
+          renderItem={renderItem}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => item.id}
+        />
       </View>
-
-
     </SafeAreaView>
   );
 };
@@ -87,8 +166,9 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 24,
   },
   body: {
-    backgroundColor: 'green',
-    height: '100%',
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 16,
   },
   transactionContainer: {
     width: '100%',
@@ -127,9 +207,6 @@ const styles = StyleSheet.create({
   walletImg: {
     width: imgContainerSize / 2,
     height: imgContainerSize / 2,
-    alignContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
     position: 'absolute',
     right: 10,
     bottom: 10,
@@ -142,7 +219,20 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
   },
-
+  interractImg: {
+    width: 18,
+    marginRight: 10,
+    height: 18,
+  },
+ divider: {
+  width: '20%',
+  marginTop: 20,
+  height: 4, 
+  alignSelf: 'center', 
+  borderColor: '#9c929a', 
+  borderRadius: 50,
+  backgroundColor: '#9c929a', 
+  },
   currency: {
     fontSize: 18,
     color: '#c4bec3',
@@ -151,6 +241,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 50,
+    gap: 8,
   },
   currentBalance: {
     fontSize: 48,
@@ -160,11 +251,12 @@ const styles = StyleSheet.create({
     color: '#FFFFFF8A',
     fontWeight: 'ultralight',
   },
-  lineImg:{
+  line: {
     width: '90%',
     marginTop: 20,
+    borderColor: '#FFFFFF4D',
+    borderWidth: 1,
   },
-
   sectionTitle: {
     fontSize: 20,
     lineHeight: 24.6,
@@ -179,20 +271,47 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: 'right',
   },
+  transaction: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  },
   transactionText: {
-    fontSize: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
     color: '#333',
   },
   transactionAmount: {
-    fontSize: 24,
-    fontWeight: '600',
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#000',
   },
-  transactionDate: {
-    fontSize: 12,
+  transactionTime: {
+    gap: 12,
+    fontSize: 16,
     color: '#666',
   },
-
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#4e3c49',
+    borderRadius: 12,
+    paddingVertical: 16,
+    gap: 12,
+    flex: 1,
+    marginHorizontal: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    alignContent: 'center',
+    color: '#FFF',
+    fontSize: 18,
+  },
 });
 
 export default App;
